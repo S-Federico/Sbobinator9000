3a38c1bbca320c65c1cdde4764674127ccff25b7
LoZimmero <fiorettigabriele@gmail.com>
2022-05-03 23:06:57 +0200

    Added first UI with minimun control on record button

--- app/src/main/java/com/imotorini/sbobinator9000/MainActivity.java
+++ app/src/main/java/com/imotorini/sbobinator9000/MainActivity.java
@@ -1,14 +1,47 @@
 package com.imotorini.sbobinator9000;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton recorderButton;
+    private TextView isRecordingTextView;
+    private boolean isRecording = false;
+
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recorderButton = findViewById(R.id.recorder_button);
+        isRecordingTextView = findViewById(R.id.is_recording_tv);
+
+        initializeWidgets();
+        updateUI();
+
+    }
+
+    // This method should be called when UI updates
+    private void updateUI() {
+        isRecordingTextView.setText(isRecording ? "Recording" : "Not recording");
+    }
+
+    private void initializeWidgets() {
+        recorderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Star recorder service
+                isRecording = !isRecording;
+                updateUI();
+            }
+        });
     }
 }
