903da5cb9517899aaa240f30b3b1c421c8f029e4
LoZimmero <fiorettigabriele@gmail.com>
2022-09-29 21:37:57 +0200

    Refactor

--- app/src/main/java/com/imotorini/sbobinator9000/MainActivity.java
+++ app/src/main/java/com/imotorini/sbobinator9000/MainActivity.java
@@ -19,14 +19,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCrate called");
         setContentView(R.layout.activity_main);
-
-        recorderButton = findViewById(R.id.recorder_button);
-        isRecordingTextView = findViewById(R.id.is_recording_tv);
 
         initializeWidgets();
         updateUI();
-
     }
 
     // This method should be called when UI updates
@@ -34,10 +31,18 @@ public class MainActivity extends AppCompatActivity {
         isRecordingTextView.setText(isRecording ? "Recording" : "Not recording");
     }
 
+    // In this method there should be set ui elements configurations
     private void initializeWidgets() {
+
+        // Get UI elements references
+        recorderButton = findViewById(R.id.recorder_button);
+        isRecordingTextView = findViewById(R.id.is_recording_tv);
+
+        // Setup recorder button to start/stop recording when pressed
         recorderButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d(TAG, "Recording button pressed");
                 // Star recorder service
                 isRecording = !isRecording;
                 updateUI();
