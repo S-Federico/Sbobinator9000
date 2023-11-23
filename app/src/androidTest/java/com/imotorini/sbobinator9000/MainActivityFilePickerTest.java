f6b1d3ee5cbefea6fa348b2157a880a035a371fa
jessicacinelli <jessicacinelli98@gmail.com>
2023-11-23 15:56:44 +0100

    fix FilePicker test

--- app/src/androidTest/java/com/imotorini/sbobinator9000/MainActivityFilePickerTest.java
+++ app/src/androidTest/java/com/imotorini/sbobinator9000/MainActivityFilePickerTest.java
@@ -3,62 +3,71 @@ package com.imotorini.sbobinator9000;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import android.app.Instrumentation;
 import android.content.Intent;
+import android.net.Uri;
 
-import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.matcher.IntentMatchers;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.After;
 import org.junit.Before;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import androidx.test.rule.GrantPermissionRule;
 
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityFilePickerTest {
+
+    public ActivityScenario<MainActivity> activityScenario;
     @Rule
-    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+    public GrantPermissionRule mGrantPermissionRule =
+            GrantPermissionRule.grant(
+                    "android.permission.RECORD_AUDIO",
+                    "android.permission.READ_EXTERNAL_STORAGE",
+                    //"android.permission.WRITE_EXTERNAL_STORAGE", --> This permission crashes the test
+                    "android.permission.CHANGE_WIFI_MULTICAST_STATE",
+                    "android.permission.ACCESS_WIFI_STATE");
 
     @Before
     public void setUp() {
-        activityRule.getScenario().moveToState(Lifecycle.State.RESUMED);
-    }
+        Intents.init();
+        activityScenario = ActivityScenario.launch(MainActivity.class);
+        activityScenario.onActivity(activity -> activity.enableTranscribeButton());
+         }
 
     @After
     public void tearDown() {
-
+        activityScenario.close();
+        Intents.release();
     }
 
 
     @Test
     public void testFilePickerActivityResultHandling() {
-        Intents.init();
-        // Rendi il pulsante visibile e abilitato
-        activityRule.getScenario().onActivity(activity -> {
-            activity.enableTranscribeButton();
-            activity.onResume();
-        });
+        onView(withId(R.id.transcribe)).perform(click());
 
-        // Configura un intent fittizio per la tua attività di scelta file
-        intending(hasAction(Intent.ACTION_GET_CONTENT))
-                .respondWith(new Instrumentation.ActivityResult(MainActivity.RESULT_OK, null));
+        Intent resultIntent = new Intent();
+        Uri fileUri = Uri.parse("content://Music/recordings"); // Sostituisci con il percorso effettivo del tuo file
+        resultIntent.setData(fileUri);
 
-        onView(ViewMatchers.withId(R.id.transcribe)).perform(click());
+        Intents.intending(hasAction(Intent.ACTION_GET_CONTENT)).respondWith(new Instrumentation.ActivityResult(MainActivity.RESULT_OK, resultIntent));
+        // Verifica che sia stata creata un'intenzione per scegliere il file
+        //intending(hasAction(Intent.ACTION_GET_CONTENT)).respondWith(new Instrumentation.ActivityResult(MainActivity.RESULT_OK, null));
+        Intents.intended(IntentMatchers.hasAction(Intent.ACTION_CHOOSER));
+        // Verifica il testo del chooser
+       // Intents.intended(IntentMatchers.hasAction(Intent.ACTION_CHOOSER));
 
-        // Simula la scelta di un file
-        // Verifica che sia stato creato e avviato un intent con le corrette azioni e categorie
-        Intents.intended(IntentMatchers.anyIntent());
-        // Termina la registrazione degli intenti
-        Intents.release();
 
     }
 
